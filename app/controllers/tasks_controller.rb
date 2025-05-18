ef25c28e74a80115a4851bff6585cf243992959c
Kitikun <kitikun.po@gmail.com>
2025-05-18 21:23:42 +0700

    add method index, create, destroy, toggle, set_task, task_params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [ :destroy, :toggle ]
+
+  def index
+    @tasks = Task.order(created_at: :desc)
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to root_path }
+      end
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
+
+  def toggle
+    @task.update(completed: !@task.completed)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
